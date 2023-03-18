486e7f790aa21b464b9cffa5988a5b2963af9975
Ayush-Prasad4 <89988134+Ayush-Prasad4@users.noreply.github.com>
2023-03-18 15:45:57 +0530

    Initial Commit

---
+++ WarCard/WarCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardApp.swift
+//  WarCard
+//
+//  Created by Ayush Prasad on 18/03/23.
+//
+
+import SwiftUI
+
+@main
+struct WarCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
